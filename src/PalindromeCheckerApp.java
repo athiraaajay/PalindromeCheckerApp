39a01b7afa0748d471e845bfd739279f2d55db38
athiraajay <athiraajay2005@gmail.com>
2026-03-09 10:16:39 +0530

    UC8:Linked List based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,15 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class PalindromeCheckerApp {
+
+
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -9,22 +17,21 @@ public class PalindromeCheckerApp {
         String original = "racecar";
 
 
-        Deque<Character> deque = new ArrayDeque<>();
-
-
+        Node head = null;
+        Node tail = null;
         for (int i = 0; i < original.length(); i++) {
-            deque.addLast(original.charAt(i));
-        }
-
-        boolean isPalindrome = true;
-
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
+            Node newNode = new Node(original.charAt(i));
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
+
+
+        boolean isPalindrome = checkPalindrome(head);
 
 
         if (isPalindrome) {
@@ -32,5 +39,53 @@ public class PalindromeCheckerApp {
         } else {
             System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
         }
+    }
+
+
+    private static boolean checkPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
+
+
+        Node slow = head;
+        Node fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+
+        Node secondHalf = reverseList(slow.next);
+
+
+        Node firstHalf = head;
+        Node tempSecond = secondHalf;
+        boolean result = true;
+        while (tempSecond != null) {
+            if (firstHalf.data != tempSecond.data) {
+                result = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            tempSecond = tempSecond.next;
+        }
+
+
+        slow.next = reverseList(secondHalf);
+
+        return result;
+    }
+
+
+    private static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
     }
 }
