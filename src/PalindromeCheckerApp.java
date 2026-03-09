a5e89ef2ed7856bfbc9e2d43b3aebb3b51415303
athiraajay <athiraajay2005@gmail.com>
2026-03-09 10:19:36 +0530

    UC9:Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,38 +1,12 @@
 public class PalindromeCheckerApp {
 
-
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
     public static void main(String[] args) {
 
 
-        String original = "racecar";
+        String original = "madam";
 
 
-        Node head = null;
-        Node tail = null;
-        for (int i = 0; i < original.length(); i++) {
-            Node newNode = new Node(original.charAt(i));
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-
-        boolean isPalindrome = checkPalindrome(head);
-
+        boolean isPalindrome = isPalindromeRecursive(original, 0, original.length() - 1);
 
         if (isPalindrome) {
             System.out.println("The string \"" + original + "\" is a Palindrome.");
@@ -42,50 +16,18 @@ public class PalindromeCheckerApp {
     }
 
 
-    private static boolean checkPalindrome(Node head) {
-        if (head == null || head.next == null) return true;
+    private static boolean isPalindromeRecursive(String str, int start, int end) {
 
-
-        Node slow = head;
-        Node fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if (start >= end) {
+            return true;
         }
 
 
-        Node secondHalf = reverseList(slow.next);
-
-
-        Node firstHalf = head;
-        Node tempSecond = secondHalf;
-        boolean result = true;
-        while (tempSecond != null) {
-            if (firstHalf.data != tempSecond.data) {
-                result = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            tempSecond = tempSecond.next;
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
 
 
-        slow.next = reverseList(secondHalf);
-
-        return result;
-    }
-
-
-    private static Node reverseList(Node head) {
-        Node prev = null;
-        Node current = head;
-        Node next = null;
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        return prev;
+        return isPalindromeRecursive(str, start + 1, end - 1);
     }
 }
